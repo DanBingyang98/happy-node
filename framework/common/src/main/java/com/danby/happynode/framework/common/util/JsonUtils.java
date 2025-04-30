7cde2a88f98da8be06ef8589eca1950526624125
danbingyang <danby98@163.com>
2025-04-30 20:21:43 +0800

    add common JsonUtils

---
+++ framework/common/src/main/java/com/danby/happynode/framework/common/util/JsonUtils.java
@@ -0,0 +1,29 @@
+package com.danby.happynode.framework.common.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.SneakyThrows;
+
+public class JsonUtils {
+    // 定义一个静态的 ObjectMapper 对象
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    // 静态代码块，用于初始化 ObjectMapper 对象
+    static {
+        // 设置 ObjectMapper 对象在反序列化时，如果遇到未知属性，不抛出异常
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 设置 ObjectMapper 对象在序列化时，如果遇到空对象，不抛出异常
+        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // 解决 LocalDateTime 的序列化问题
+        OBJECT_MAPPER.registerModules(new JavaTimeModule());
+    }
+
+    @SneakyThrows
+    // 将对象转换为JSON字符串
+    public static String toJsonString(Object obj) {
+        // 使用OBJECT_MAPPER将对象转换为JSON字符串
+        return OBJECT_MAPPER.writeValueAsString(obj);
+    }
+}
