c8d1228e52ca765e3e9eb378b1cf45bf149964e0
danbingyang <danby98@163.com>
2025-06-20 20:57:29 +0800

    develop count like note

--- happynode-note/happynode-note-biz/src/main/java/com/danby/happynode/note/biz/consumer/LikeUnlikeNoteConsumer.java
+++ happynode-note/happynode-note-biz/src/main/java/com/danby/happynode/note/biz/consumer/LikeUnlikeNoteConsumer.java
@@ -7,18 +7,22 @@ import com.danby.happynode.note.biz.domain.dataobject.NoteLikeDO;
 import com.danby.happynode.note.biz.domain.mapper.NoteLikeDOMapper;
 import com.danby.happynode.note.biz.model.dto.LikeUnlikeNoteMqDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Component
-@RocketMQMessageListener(consumerGroup = "xiaohashu_group_" + MQConstants.TOPIC_LIKE_OR_UNLIKE, // Group 组
+@RocketMQMessageListener(consumerGroup = "happynode_group_" + MQConstants.TOPIC_LIKE_OR_UNLIKE, // Group 组
         topic = MQConstants.TOPIC_LIKE_OR_UNLIKE, // 消费的主题 Topic
         consumeMode = ConsumeMode.ORDERLY // 设置为顺序消费模式
 )
@@ -30,6 +34,8 @@ public class LikeUnlikeNoteConsumer implements RocketMQListener<Message> {
 
     @Autowired
     private NoteLikeDOMapper noteLikeDOMapper;
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
 
     @Override
     public void onMessage(Message message) {
@@ -74,7 +80,23 @@ public class LikeUnlikeNoteConsumer implements RocketMQListener<Message> {
                 .build();
         // 添加或更新笔记点赞记录
         int count = noteLikeDOMapper.insertOrUpdate(noteLikeDO);
-        // TODO: 发送计数 MQ
+        // 发送计数 MQ
+        if (count > 0) {
+            org.springframework.messaging.Message<String> message = MessageBuilder.withPayload(bodyJsonStr).build();
+            rocketMQTemplate.asyncSend(MQConstants.TOPIC_COUNT_NOTE_LIKE, message, new SendCallback() {
+
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    log.info("==> 【计数: 笔记点赞】MQ 发送成功，SendResult: {}", sendResult);
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+                    log.error("==> 【计数: 笔记点赞】MQ 发送异常: ", throwable);
+                }
+            });
+        }
+
     }
 
 
@@ -99,6 +121,20 @@ public class LikeUnlikeNoteConsumer implements RocketMQListener<Message> {
                 .build();
         // 取消点赞：记录更新
         int count = noteLikeDOMapper.update2UnlikeByUserIdAndNoteId(noteLikeDO);
-        // TODO: 发送计数 MQ
+        // 发送计数 MQ
+        if (count > 0) {
+            org.springframework.messaging.Message<String> message = MessageBuilder.withPayload(bodyJsonStr).build();
+            rocketMQTemplate.asyncSend(MQConstants.TOPIC_COUNT_NOTE_LIKE, message, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    log.info("==> 【计数: 笔记取消点赞】MQ 发送成功，SendResult: {}", sendResult);
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+                    log.error("==> 【计数: 笔记取消点赞】MQ 发送异常: ", throwable);
+                }
+            });
+        }
     }
 }
