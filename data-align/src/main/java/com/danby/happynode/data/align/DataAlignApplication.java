3c796ca65a97aaeb622b0ef44885b333d49b9a9d
danbingyang <danby98@163.com>
2025-06-22 19:46:06 +0800

    develop data align like

--- data-align/src/main/java/com/danby/happynode/data/align/DataAlignApplication.java
+++ data-align/src/main/java/com/danby/happynode/data/align/DataAlignApplication.java
@@ -1,11 +1,13 @@
 package com.danby.happynode.data.align;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.danby.happynode.data.align.domain.mapper")
 public class DataAlignApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataAlignApplication.class, args);
